50a428ee8fabca866ff81a1e8798c57d44a7519b
Jose Luiz Rodrigues <zeluizr@me.com>
2024-04-12 20:21:56 -0400

    Update package.json version to 0.0.4

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        return credentials;
+      },
+    }),
+  ],
+  pages: { error: "/error" },
+});
